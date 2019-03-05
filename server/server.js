1041aceed6b8da3e3edfbebfb01698033614f243
CanonManF22 <avi.banerjee164@gmail.com>
2019-03-05 12:52:40 -0800

    need to add DB mysql connection

--- server/server.js
+++ server/server.js
@@ -10,5 +10,5 @@ app.use(
 app.use(bodyParser.json());
 //TODO: ADD DB Config
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
