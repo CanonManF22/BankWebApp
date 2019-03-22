74bf9befc6af8e08883fee043895866c42da7f3b
CanonManF22 <avi.banerjee164@gmail.com>
2019-03-22 14:27:31 -0700

    added routing after successful login, included db queries in users apis

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
-
 class LoginForm extends React.Component {
   state = {
     data: {
@@ -11,25 +10,13 @@ class LoginForm extends React.Component {
     errors: {}
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    // const response = await fetch('/api/hello').then(res => res.json());
-    // if (response.status !== 200) throw Error(response.message);
-    // console.log(response)
-    // return body;
-  };
-
   onChange = e =>
     this.setState({
       data: { ...this.state.data, [e.target.name]: e.target.value }
     });
 
   handleSubmit = async e => {
+    console.log('handling submit')
     e.preventDefault();
     const { data } = this.state;
     const response = await fetch('http://localhost:8080/users/login', {
@@ -41,6 +28,11 @@ class LoginForm extends React.Component {
       body: JSON.stringify({ post: data })
     }).then(res => res.json());
     this.setState({ responseToPost: response });
+    console.log(response)
+    //navigate to next page
+    if(response != null){
+      window.location.href = ''
+    }
   };
 
   render() {
