0387448dd5cb86b92fa137f57e79fac95df8f81a
CanonManF22 <avi.banerjee164@gmail.com>
2019-03-23 23:14:13 -0700

    working on AccountPage. Making fetchAccounts function that gets all accounts of user. How do we know user_id?

--- client/src/components/pages/AccountPage.js
+++ client/src/components/pages/AccountPage.js
@@ -7,11 +7,68 @@ const divStyle = {
   marginTop: "0px"
 };
 
+
+class Page extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+
+  
+  renderAccount(i){
+    return (
+    <div>
+      temp
+    </div>
+    );
+  }
+
+  //gets all accounts of a given user_id
+  fetchAccounts(user_id) = async e => {
+    console.log('finding accounts under ', user_id);
+    e.preventDefault();
+    const { data } = this.state;
+    const response = await fetch("http://localhost:8080/accounts/:user_id/", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ post: data })
+    }).then(res => res.json());
+    this.setState({ responseToPost: response });
+    if (response.Success) {
+      window.location.href = "/dashboard";
+    }
+    else{
+      //wrong pw, try again, maybe include alert
+      window.location.href = "/";
+    }
+    
+  };
+
+  
+  //fetches account info from api, renders each account, where do we get user id from?
+  render() {
+    const user_id = '';
+    const {accounts} = this.fetchAccounts(user_id);
+    return(
+      <div>
+        {this.renderAccount(0)}
+        {this.renderAccount(1)}
+      </div>
+    );
+  }
+}
+
 class AccountPage extends React.Component {
   render() {
     return (
       <div style={divStyle}>
-        <h1 style={{ color: "#ffffff" }}>Placeholder [Account]</h1>
+        <Page/>
       </div>
     );
   }
