eb59e4bf0395e4744e77f144807c80e0501e48c0
Andrea Hernandez <andreaeu02@gmail.com>
2019-03-25 22:46:06 -0700

    add SQL code that will not add a user with an existing username or existing email, so far just the data base won't add it. It needs to add some front end pop up saying that to the user

--- server/users.js
+++ server/users.js
@@ -38,7 +38,14 @@ router.post('/register', (req, res) => {
         console.log(err);
       } else {
         console.log(hash.length);
-        const sql = `INSERT INTO Users(username, password, email, firstname, lastname) VALUES ('${username}', '${hash}', '${email}', '${firstName}', '${lastName}')`;
+        //const sql = `INSERT INTO Users(username, password, email, firstname, lastname) VALUES ('${username}', '${hash}', '${email}', '${firstName}', '${lastName}')`;
+        
+        const sql = `INSERT INTO bank.Users (username, password, email, firstname, lastname)
+                      SELECT * FROM (SELECT '${username}', '${hash}', '${email}', '${firstName}', '${lastName}') AS tmp
+                      WHERE NOT EXISTS (
+                      SELECT username, email FROM bank.Users WHERE username = '${username}' OR email = '${email}'
+                      )LIMIT 1`;
+                      
         console.log(sql);
         const query = db.query(sql, (err, result) => {
           if (err) throw err;
