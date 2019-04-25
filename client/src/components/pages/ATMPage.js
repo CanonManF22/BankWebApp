6338ddc1cc4be0d483f632dfc176ad45e68ba793
speedypotato <speeedypotato@gmail.com>
2019-04-24 17:13:39 -0700

    implemented map relocation via address

--- client/src/components/pages/ATMPage.js
+++ client/src/components/pages/ATMPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Geocode from "react-geocode";
+import ReactDOM from "react-dom";
 import { Form, Button } from "react-bootstrap";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const divStyle = {
+  position: "relative",
   height: "167vh",
   backgroundColor: "#c1ced9",
   padding: "50px",
@@ -15,18 +17,37 @@ export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      address: null,
+      address: "1 Washington Square",
       address2: null,
-      city: null,
-      statename: null,
-      zip: null,
-      lat: null,
-      long: null
+      city: "San Jose",
+      statename: "CA",
+      zip: "95192",
+      lat: 37.335407,
+      long: -121.881244
     };
     // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
     Geocode.setApiKey("AIzaSyDSbKUp1Xu9yP6FUwvjcj701S_NtR0N1Po");
     // Enable or disable logs. Its optional.
     Geocode.enableDebug();
+  }
+
+  componentDidMount() {
+    if (this.props && this.props.google) {
+      // google is available
+      const { google } = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      const mapConfig = {
+        center: { lat: this.state.lat, lng: this.state.long },
+        zoom: 14
+      };
+      this.map = new maps.Map(node, mapConfig);
+    } else {
+      window.alert("Error, couldn't reach Google Maps.");
+    }
   }
 
   // Get latidude & longitude from address and set gmaps
@@ -41,11 +62,17 @@ export class MapContainer extends React.Component {
       this.state.statename +
       "," +
       this.state.zip;
-    let latLong = { lat: 0, long: 0 };
     Geocode.fromAddress(tempAddr).then(
       response => {
         let latLong = response.results[0].geometry.location;
         this.setState({ lat: latLong.lat, long: latLong.long });
+
+        const mapConfig = {
+          center: latLong,
+          zoom: 14
+        };
+        this.map = new maps.Map(node, mapConfig);
+
         console.log(latLong);
       },
       error => {
@@ -53,7 +80,27 @@ export class MapContainer extends React.Component {
         console.error(error);
       }
     );
-    return latLong;
+
+    // google is available
+    const { google } = this.props;
+    const maps = google.maps;
+
+    const mapRef = this.refs.map;
+    const node = ReactDOM.findDOMNode(mapRef);
+
+    /* working test coords
+    const templat = 35.658072;
+    const templng = 139.70164;
+
+    const mapConfig = {
+      center: {
+        lat: parseFloat(templat),
+        lng: parseFloat(templng)
+      },
+      zoom: 14
+    };
+    this.map = new maps.Map(node, mapConfig);
+    */
   };
 
   handleChangeAddr = e => {
@@ -93,8 +140,7 @@ export class MapContainer extends React.Component {
             <Form.Label>Address 1</Form.Label>
             <Form.Control
               type="text"
-              value={this.state.address}
-              placeholder={"1234 Main St"}
+              placeholder={"1 Washington Square"}
               onChange={this.handleChangeAddr}
             />
           </Form.Group>
@@ -102,35 +148,31 @@ export class MapContainer extends React.Component {
             <Form.Label>Address 2</Form.Label>
             <Form.Control
               type="text"
-              value={this.state.address2}
               placeholder={"Apartment, studio, or floor"}
               onChange={this.handleChangeAddr2}
             />
           </Form.Group>
           <Form.Row>
-            <Form.Group class={"col-md-6"}>
+            <Form.Group className={"col-md-4"}>
               <Form.Label>City</Form.Label>
               <Form.Control
                 type="text"
-                value={this.state.city}
                 placeholder={"San Jose"}
                 onChange={this.handleChangeCity}
               />
             </Form.Group>
-            <Form.Group class={"col-md-4"}>
+            <Form.Group className={"col-md-2"}>
               <Form.Label>State</Form.Label>
               <Form.Control
                 type="text"
-                value={this.state.statename}
                 placeholder={"CA"}
                 onChange={this.handleChangeState}
               />
             </Form.Group>
-            <Form.Group class={"col-md-2"}>
+            <Form.Group className={"col-md-2"}>
               <Form.Label>Zip</Form.Label>
               <Form.Control
                 type="text"
-                value={this.state.zip}
                 placeholder={"95192"}
                 onChange={this.handleChangeZip}
               />
@@ -148,17 +190,7 @@ export class MapContainer extends React.Component {
             </Button>
           </Form.Row>
         </Form.Group>
-        <Map
-          style={{
-            width: "900px",
-            height: "900px"
-          }}
-          google={this.props.google}
-          zoom={14}
-        >
-          <Marker onClick={this.onMarkerClick} name={"Current location"} />
-          <InfoWindow onClose={this.onInfoWindowClose} />
-        </Map>
+        <Map ref={"map"} google={this.props.google} zoom={14} />
       </div>
     );
   }
